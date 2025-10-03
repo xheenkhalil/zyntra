28ce586eef2d4cd1727e20f1ab8ec591918573cd
Moses Thomas <78230723+xheenkhalil@users.noreply.github.com>
2025-10-03 20:12:55 +0100

    Add superAdmin configuration file

---
+++ backend/src/config/superAdmin.js
@@ -0,0 +1,7 @@
+export const superAmin = {
+  email: 
+    process.env.SUPER_ADMIN_EMAIL || "superadmin@zyntra.com",
+  password:
+    process.env.SUPER_ADMIN_PASSWORD || " $argon2id$v=19$m=65536,t=3,p=4$BgHn51ZrsyoCt6miBieWWA$IP1aRnVHArOgN9B8Y/PkO+9U6c20SJMyYdXdZpBpFWQ "
+  role: "superadmin"
+};
