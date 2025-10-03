e1ee9a0d3bc98dc1e0cd438fb8ba7cc6f6c03cc6
Moses Thomas <78230723+xheenkhalil@users.noreply.github.com>
2025-10-03 20:24:29 +0100

    Update superAdmin.js

--- backend/src/config/superAdmin.js
+++ backend/src/config/superAdmin.js
@@ -4,5 +4,3 @@ export const superAdmin = {
   password:
     process.env.SUPER_ADMIN_PASSWORD || " $argon2id$v=19$m=65536,t=3,p=4$BgHn51ZrsyoCt6miBieWWA$IP1aRnVHArOgN9B8Y/PkO+9U6c20SJMyYdXdZpBpFWQ ",
 };
-
-export default superAdmin;
