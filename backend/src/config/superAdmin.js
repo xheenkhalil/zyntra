e892d22593e60a761984456c96a6af5545fb3f91
Moses Thomas <78230723+xheenkhalil@users.noreply.github.com>
2025-10-03 20:15:46 +0100

    Remove role property from superAdmin configuration

--- backend/src/config/superAdmin.js
+++ backend/src/config/superAdmin.js
@@ -3,5 +3,4 @@ export const superAmin = {
     process.env.SUPER_ADMIN_EMAIL || "superadmin@zyntra.com",
   password:
     process.env.SUPER_ADMIN_PASSWORD || " $argon2id$v=19$m=65536,t=3,p=4$BgHn51ZrsyoCt6miBieWWA$IP1aRnVHArOgN9B8Y/PkO+9U6c20SJMyYdXdZpBpFWQ ",
-  role: "superadmin"
 };
