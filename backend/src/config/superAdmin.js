fb9a014ef3905d1cf20664c1c23fc831ccef48b6
Moses Thomas <78230723+xheenkhalil@users.noreply.github.com>
2025-10-03 20:38:59 +0100

    Delete superAdmin configuration file
    Remove superAdmin configuration from the project.

--- backend/src/config/superAdmin.js
+++ backend/src/config/superAdmin.js
@@ -1,6 +1,1 @@
-export const superAdmin = {
-  email: 
-    process.env.SUPER_ADMIN_EMAIL || "superadmin@zyntra.com",
-  password:
-    process.env.SUPER_ADMIN_PASSWORD || " $argon2id$v=19$m=65536,t=3,p=4$BgHn51ZrsyoCt6miBieWWA$IP1aRnVHArOgN9B8Y/PkO+9U6c20SJMyYdXdZpBpFWQ ",
-};
+
